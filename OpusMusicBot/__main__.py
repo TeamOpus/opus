dffa06c376f72bdf77e5356e2720dc32c130eb68
TeamOpus <srvopus@gmail.com>
2025-07-18 15:23:39 +0530

    Create __main__.py

---
+++ OpusMusicBot/__main__.py
@@ -0,0 +1,18 @@
+from OpusMusicBot.core.bot import app
+from OpusMusicBot.core.userbot import userbot
+from OpusMusicBot.call import Anony
+
+from pyrogram import idle
+import asyncio
+
+async def run():
+    await app.start()
+    await userbot.start()
+    await Anony.start()
+    print("OpusMusicBot is up and running.")
+    await idle()
+    await app.stop()
+    await userbot.stop()
+
+if __name__ == "__main__":
+    asyncio.run(run())
