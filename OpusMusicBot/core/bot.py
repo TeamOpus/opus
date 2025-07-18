0e1ccdf4ccf2dde391ea8693f44cecfd786d7a06
TeamOpus <srvopus@gmail.com>
2025-07-18 15:26:16 +0530

    Create bot.py

---
+++ OpusMusicBot/core/bot.py
@@ -0,0 +1,11 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+app = Client(
+    "OpusMusicBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    workers=20,
+    plugins=dict(root="OpusMusicBot.plugins")
+)
