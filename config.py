16355aedb450cecf87c20d9acd00e35c7d0b9347
TeamOpus <srvopus@gmail.com>
2025-07-18 17:57:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Environment variables with default fallback values
-API_ID = int(os.getenv("API_ID", 12345))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-MONGO_URI = os.getenv("MONGO_URI", "your_mongo_uri")
-STRING_SESSION = os.getenv("STRING_SESSION", "your_string_session")
-SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "BillaCore")
-OWNER_ID = int(os.getenv("OWNER_ID", 5960968099))
-API_URL = os.getenv("API_URL", None)
+API_ID = int(getenv("API_ID", 12345))
+API_HASH = getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = getenv("BOT_TOKEN", "your_bot_token")
+MONGO_URI = getenv("MONGO_URI", "your_mongo_uri")
+STRING_SESSION = getenv("STRING_SESSION", "your_string_session")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "BillaCore")
+OWNER_ID = int(getenv("OWNER_ID", 5960968099))
+API_URL = getenv("API_URL", None)
+WEB_URL = getenv("WEB_URL", None)
 
 def check_env_vars():
     """Check if environment variables are set to non-default values and issue warnings if not."""
